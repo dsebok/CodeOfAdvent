f850cd689f8d7adf8c8335eda42c601289167b13
David Sebok <dsebok@gmail.com>
2018-12-06 20:29:48 +0100

    data is scanned to an ArrayList

--- stars_1-10/src/Day_1_ChronalCalibration.java
+++ stars_1-10/src/Day_1_ChronalCalibration.java
@@ -1,24 +1,40 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Day_1_ChronalCalibration {
 
-	private static void last_frequency(Scanner sc) {
+	private static void populate_list(Scanner sc, ArrayList<Integer> freqMods) {
+		while (sc.hasNextLine()) {
+			freqMods.add(Integer.parseInt(sc.nextLine()));
+		}
+	}
+	
+	private static void last_freq(Scanner sc, ArrayList<Integer> freqMods) {
+		int frequency = 0;
+		for (Integer freqMod: freqMods) {
+			frequency += freqMod;
+		}
+		System.out.println(frequency);
+	}
+	
+	private static void first_double_freq(Scanner sc) {
 		int frequency = 0;
 		while (sc.hasNextLine()) {
-			int i = Integer.parseInt(sc.nextLine());
-	        frequency += i;
-	        System.out.println(frequency);
-	        }	
+			int freqMod = Integer.parseInt(sc.nextLine());
+	        frequency += freqMod;
+	        }
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		File file = new File("D:\\Code_Life\\repos\\CodeOfAdvent\\input.txt");
-		Scanner sc=new Scanner(file);
+		Scanner sc = new Scanner(file);
+		ArrayList<Integer> freqMods = new ArrayList<Integer>();
 		sc.useDelimiter("\\Z");
-		last_frequency(sc);
+		populate_list(sc, freqMods);
+		last_freq(sc, freqMods);
 	}
 	
 }
