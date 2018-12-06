08b413c9c7454ac3c42d97f138a40fd420c23cfc
David Sebok <dsebok@gmail.com>
2018-12-06 14:35:26 +0100

    reading from txt

--- stars_1-10/src/Day_1_ChronalCalibration.java
+++ stars_1-10/src/Day_1_ChronalCalibration.java
@@ -1,9 +1,25 @@
-
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+		
 public class Day_1_ChronalCalibration {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		File file = new File("D:\\Code_Life\\repos\\CodeOfAdvent\\input.txt");
 
+	    try {
+
+	        Scanner sc = new Scanner(file);
+
+	        while (sc.hasNextLine()) {
+	            int i = sc.nextInt();
+	            System.out.println(i);
+	        }
+	        sc.close();
+	    } 
+	    catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    }
 	}
-
 }
