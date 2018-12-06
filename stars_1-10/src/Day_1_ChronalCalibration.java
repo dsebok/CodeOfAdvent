29193d64bf029c328fb5c14bbcfbd6a2fe39745d
David Sebok <dsebok@gmail.com>
2018-12-06 19:09:13 +0100

    error msg corrected

--- stars_1-10/src/Day_1_ChronalCalibration.java
+++ stars_1-10/src/Day_1_ChronalCalibration.java
@@ -1,31 +1,24 @@
 import java.io.File;
-import java.util.Scanner;
 import java.io.FileNotFoundException;
-		
+import java.util.Scanner;
+
 public class Day_1_ChronalCalibration {
 
 	private static void last_frequency(Scanner sc) {
 		int frequency = 0;
 		while (sc.hasNextLine()) {
-			int i = sc.nextInt();
+			int i = Integer.parseInt(sc.nextLine());
 	        frequency += i;
 	        System.out.println(frequency);
-	        }
-		System.out.println(frequency);
-		sc.close();	
+	        }	
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
 		File file = new File("D:\\Code_Life\\repos\\CodeOfAdvent\\input.txt");
-
-	    try {
-	        Scanner sc = new Scanner(file);
-	        last_frequency(sc);
-	    }
-	    
-	    catch (FileNotFoundException e) {
-	        e.printStackTrace();
-	    }
+		Scanner sc=new Scanner(file);
+		sc.useDelimiter("\\Z");
+		last_frequency(sc);
 	}
+	
 }
