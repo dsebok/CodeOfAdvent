dde471f294c114ce33939d1fd1a5ab91fbc8fe76
David Sebok <dsebok@gmail.com>
2018-12-06 18:02:49 +0100

    frequency is calculated via a void function

--- stars_1-10/src/Day_1_ChronalCalibration.java
+++ stars_1-10/src/Day_1_ChronalCalibration.java
@@ -4,20 +4,26 @@ import java.io.FileNotFoundException;
 		
 public class Day_1_ChronalCalibration {
 
+	private static void last_frequency(Scanner sc) {
+		int frequency = 0;
+		while (sc.hasNextLine()) {
+			int i = sc.nextInt();
+	        frequency += i;
+	        System.out.println(frequency);
+	        }
+		System.out.println(frequency);
+		sc.close();	
+	}
+	
 	public static void main(String[] args) {
 		
 		File file = new File("D:\\Code_Life\\repos\\CodeOfAdvent\\input.txt");
 
 	    try {
-
 	        Scanner sc = new Scanner(file);
-
-	        while (sc.hasNextLine()) {
-	            int i = sc.nextInt();
-	            System.out.println(i);
-	        }
-	        sc.close();
-	    } 
+	        last_frequency(sc);
+	    }
+	    
 	    catch (FileNotFoundException e) {
 	        e.printStackTrace();
 	    }
