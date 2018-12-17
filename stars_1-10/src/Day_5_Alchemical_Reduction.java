d1475fb4f49b7363365851b02e269be51da632d5
David Sebok <dsebok@gmail.com>
2018-12-17 19:39:42 +0100

    day5part2 delAllChars

--- stars_1-10/src/Day_5_Alchemical_Reduction.java
+++ stars_1-10/src/Day_5_Alchemical_Reduction.java
@@ -26,6 +26,18 @@ public class Day_5_Alchemical_Reduction {
 		return false;
 	}
 	
+	private static String delChars(String inputStr, int delStart, int delEnd) {
+		return inputStr.substring(0, delStart) + inputStr.substring(delEnd + 1, inputStr.length());
+	}
+	
+	private static int iReduce(int i) {
+		if (i == 0) {
+			return -1;
+		} else {
+			return i-2;
+		}
+	}
+	
 	private static String unitReaction(String polymerBuild) {
 		char firstChar;
 		char secondChar;
@@ -34,14 +46,23 @@ public class Day_5_Alchemical_Reduction {
 			firstChar = polymerBuild.charAt(i);
 			secondChar = polymerBuild.charAt(i + 1);
 			if (areOpposites(firstChar, secondChar)) {
-				polymerBuild = polymerBuild.substring(0, i) + polymerBuild.substring(i+2, polymerBuild.length());
-				if (i == 0) {
-					i = i-1;
-				} else {
-					i = i-2;
-				}
+				polymerBuild = delChars(polymerBuild, i, i+1);
+				i = iReduce(i);
 			}
 			++i;
+		}
+		return polymerBuild;
+	}
+	
+	private static String delAllChar(String polymerBuild, char xChar) {
+		int i = -1;
+		while (++i < polymerBuild.length()) {
+			//System.out.println(i);
+			char currentChar = polymerBuild.charAt(i);
+			if (currentChar == xChar || currentChar == Character.toUpperCase(xChar)) {
+				polymerBuild = delChars(polymerBuild, i, i);
+				--i;
+			}
 		}
 		return polymerBuild;
 	}
@@ -57,6 +78,13 @@ public class Day_5_Alchemical_Reduction {
 		String unit1 = "dabCBAcaDA";
 		inputStr = unitReaction(inputStr);
 		System.out.println("Unit test 1: " + inputStr.equals(unit1));
+		
+		polymerBuild = unitReaction(polymerBuild);
+		System.out.println(polymerBuild.length());
+		
+		String abc = "abcdefghijklmnopqrstuvwxyz";
+		polymerBuild = delAllChar(polymerBuild, abc.charAt(23));
+		System.out.println(polymerBuild.length());
 
 	}
 }
