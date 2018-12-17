6849b9bd9a5b013da110122f2094839aedbb6e90
David Sebok <dsebok@gmail.com>
2018-12-17 17:44:57 +0100

    day5part1 opposites

--- stars_1-10/src/Day_5_Alchemical_Reduction.java
+++ stars_1-10/src/Day_5_Alchemical_Reduction.java
@@ -13,6 +13,24 @@ public class Day_5_Alchemical_Reduction {
 		return inputStr;
 	}
 	
+	private static String unitReaction(String polymerBuild) {
+		return " ";
+	}
+	
+	private static Boolean areOpposites(char ch1, char ch2) {
+		Boolean secondIsLower = Character.toLowerCase(ch1) == ch2;
+		Boolean secondIsUpper = Character.toUpperCase(ch1) == ch2;
+		
+		Boolean firstIsLower = Character.toLowerCase(ch2) == ch1;
+		Boolean firstIsUpper = Character.toUpperCase(ch2) == ch1;
+		if (firstIsLower && secondIsUpper) {
+			return true;
+		} else if (firstIsUpper && secondIsLower) {
+			return true;
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		File file = new File("D:\\Code_Life\\repos\\CodeOfAdvent\\stars_1-10\\src\\input_5.txt");
@@ -20,7 +38,10 @@ public class Day_5_Alchemical_Reduction {
 		sc.useDelimiter("\\Z");
 		String polymerBuild = stockUp_String(sc);
 		System.out.println(polymerBuild.length());
-		System.out.println("Unit1: " + polymerBuild.charAt(11111));
+		System.out.println("Unit1: " + areOpposites('c', 'C'));
+		System.out.println("Unit1: " + areOpposites('C', 'c'));
+		System.out.println("Unit1: " + areOpposites('c', 'c'));
+		System.out.println("Unit1: " + areOpposites('C', 'C'));
 		
 
 	}
